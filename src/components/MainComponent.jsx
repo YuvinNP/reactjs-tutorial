c751a29be8d9e2dc894b2460ff509f14e0e19417
Gihan Siriwardhana <gihan.siriwardhana@ifsworld.com>
2020-04-05 22:00:10 +0530

    second

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -9,6 +9,8 @@ class MainComponent extends Component {
     render() {
         return (
             <div style={{ display: 'flex' }}>
+
+                
                 <ItemBox title="Raththi" price="450.00" desc="l;s;khs ;lkjslk jkj jls" image={Raththi} />
                 <ItemBox title="Anchor" price="600.00" desc="Anchor milk powder" image={Raththi} />
                 <ItemBox title="Viva" price="350.00" desc="lgaoih " image={Raththi} />
